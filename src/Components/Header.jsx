164d44a9c4987b3cd46ce6073e34f17f42a5cd3b
mrsupun5670 <supun9402@gmail.com>
2025-02-01 23:18:37 +0530

    D

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 function Header() {
   return (
-    <div className="relative  sticky top-0 z-50">
+    <div className="sticky top-0 z-50">
       <div className="w-full flex flex-wrap items-center md:justify-between py-2 justify-start sm:space-y-2">
         <img src={logo} alt="Logo" className="w-32 md:w-40" />
         <div className="w-full sm:w-auto mt-4">
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
  <BrowserRouter>
     <Routes>
       <Route path="*" element={<h1>Not Found</h1>} />
-      <Route path="/" element={<CheckoutPage />} />
+      <Route path="/" element={<Index />} />
     </Routes>
  </BrowserRouter>
 )
